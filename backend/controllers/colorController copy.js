663894a2ebef632fbe07740c42c51da1f9d10a5a
Ervin A <chelovek.ap@gmail.com>
2023-10-08 15:52:17 +0300

    Colors functions added

--- backend/controllers/colorController copy.js
+++ backend/controllers/colorController copy.js
@@ -1,52 +1,52 @@
-const { Material } = require("../models/models");
+const { Color } = require("../models/models");
 const { badRequest } = "../error/ApiError";
 
-class materialController {
+class colorController {
   async create(req, res, next) {
     try {
       let { name } = req.body;
 
-      const material = await Material.create({
+      const color = await Color.create({
         name,
         icon,
       });
 
-      return res.json(material);
+      return res.json(color);
     } catch (e) {
       next(badRequest(e.message));
     }
   }
 
   async getAll(req, res) {
-    const materials = await Material.findAll();
-    return res.json(materials);
+    const colors = await Color.findAll();
+    return res.json(colors);
   }
 
   async getOne(req, res) {
     const { id } = req.params;
-    const material = await Material.findOne({
+    const color = await Color.findOne({
       where: {
         id,
       },
     });
-    return res.json(material);
+    return res.json(params);
   }
 
   async deleteItem(req, res) {
     const { id } = req.params;
-    const material = await Material.destroy({
+    const color = await Color.destroy({
       where: {
         id,
       },
     });
-    return res.json(material);
+    return res.json(color);
   }
 
   async updateItem(req, res, next) {
     try {
       let { name, icon, id } = req.body;
 
-      const [updated] = await Material.update(
+      const [updated] = await Color.update(
         {
           name,
           icon,
@@ -59,7 +59,7 @@ class materialController {
       );
 
       if (updated === 0) {
-        return res.status(404).send({ error: "Material not found" });
+        return res.status(404).send({ error: "Color not found" });
       }
 
       return res.json(updated);
@@ -69,4 +69,4 @@ class materialController {
   }
 }
 
-module.exports = new materialController();
+module.exports = new colorController();
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 import Router from "express";
 const router = new Router();
 const materialRouter = require("./materialRouter");
+const colorRouter = require("./colorRouter");
 
 router.use("/material", materialRouter);
+router.use("/color", colorRouter);
 
 export default router;
