2d3699daa06fabb1dfac3213178489ca990ac4fb
Ervin A <chelovek.ap@gmail.com>
2023-10-09 12:11:16 +0300

    Added product size

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,10 @@ import Router from "express";
 const router = new Router();
 const materialRouter = require("./materialRouter");
 const colorRouter = require("./colorRouter");
+const ptoductsizeRouter = require("./producrsizeRouter");
 
 router.use("/material", materialRouter);
 router.use("/color", colorRouter);
+router.use("/productsize", ptoductsizeRouter);
 
 export default router;
