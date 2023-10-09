ffd213adde7e9513dcbcb76cefb56185ac874291
Ervin A <chelovek.ap@gmail.com>
2023-10-09 12:15:01 +0300

    Added part

---
+++ backend/controllers/partController.js
@@ -0,0 +1,72 @@
+const { Part } = require("../models/models");
+const { badRequest } = "../error/ApiError";
+
+class partController {
+  async create(req, res, next) {
+    try {
+      let { name } = req.body;
+
+      const part = await Part.create({
+        name,
+        icon,
+      });
+
+      return res.json(part);
+    } catch (e) {
+      next(badRequest(e.message));
+    }
+  }
+
+  async getAll(req, res) {
+    const parts = await Part.findAll();
+    return res.json(parts);
+  }
+
+  async getOne(req, res) {
+    const { id } = req.params;
+    const part = await Part.findOne({
+      where: {
+        id,
+      },
+    });
+    return res.json(part);
+  }
+
+  async deleteItem(req, res) {
+    const { id } = req.params;
+    const part = await Part.destroy({
+      where: {
+        id,
+      },
+    });
+    return res.json(part);
+  }
+
+  async updateItem(req, res, next) {
+    try {
+      let { name, icon, id } = req.body;
+
+      const [updated] = await Part.update(
+        {
+          name,
+          icon,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+
+      if (updated === 0) {
+        return res.status(404).send({ error: "Part not found" });
+      }
+
+      return res.json(updated);
+    } catch (e) {
+      next(badRequest(e.message));
+    }
+  }
+}
+
+module.exports = new partController();
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,11 @@ const router = new Router();
 const materialRouter = require("./materialRouter");
 const colorRouter = require("./colorRouter");
 const ptoductsizeRouter = require("./producrsizeRouter");
+const partRouter = require("./partRouter");
 
 router.use("/material", materialRouter);
 router.use("/color", colorRouter);
 router.use("/productsize", ptoductsizeRouter);
+router.use("/part", partRouter);
 
 export default router;
