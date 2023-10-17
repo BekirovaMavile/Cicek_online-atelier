c829188793e1c6f3805884938949712bbe5bf9d3
Ervin A <chelovek.ap@gmail.com>
2023-10-17 11:14:06 +0300

    product category router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,11 +5,13 @@ const colorRouter = require("./colorRouter");
 const ptoductsizeRouter = require("./producrsizeRouter");
 const partRouter = require("./partRouter");
 const partcategoryRouter = require("./partcategoryRouter");
+const productcategoryRouter = require("./productcategoryRouter");
 
 router.use("/material", materialRouter);
 router.use("/color", colorRouter);
 router.use("/productsize", ptoductsizeRouter);
 router.use("/part", partRouter);
 router.use("/partcategory", partcategoryRouter);
+router.use("/productcategory", productcategoryRouter);
 
 export default router;
