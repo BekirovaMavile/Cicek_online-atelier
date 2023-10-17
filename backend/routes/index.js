151452c9451d54cb79c64dcb7a66fff749cd3fd7
Ervin A <chelovek.ap@gmail.com>
2023-10-17 10:56:29 +0300

    part category router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,12 @@ const materialRouter = require("./materialRouter");
 const colorRouter = require("./colorRouter");
 const ptoductsizeRouter = require("./producrsizeRouter");
 const partRouter = require("./partRouter");
+const partcategoryRouter = require("./partcategoryRouter");
 
 router.use("/material", materialRouter);
 router.use("/color", colorRouter);
 router.use("/productsize", ptoductsizeRouter);
 router.use("/part", partRouter);
+router.use("/partcategory", partcategoryRouter);
 
 export default router;
---
+++ backend/routes/partcategoryRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+const router = Router();
+
+import { create, getAll, getOne, deleteItem, updateItem } from "controllers/partcategoryController.js"
+// import checkRole from "../middleware/checkRoleMiddleware";
+
+router.post("/", create);
+router.get("/", getAll);
+router.get("/:id", getOne);
+router.delete("/:id", deleteItem);
+router.put("/:id", updateItem);
+// ошибка может быть в том что id в параметрах, а не в теле запрса
+
+export default router;
+// , checkRole("ADMIN")
