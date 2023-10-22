47ed63812bc6cd3f94abba7d7a89cb0c335bcdde
Eric <chelovek.ap@icloud.com>
2023-10-22 21:22:28 +0300

    nvm

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -16,11 +16,6 @@ const {
 
 router.post("/", create);
 router.get("/", getAll);
-// router.get("/:id", getOne);
-// router.get("/:color_id", getByColor);
-// router.get("/:material_id", getByMaterial);
-// router.get("/:product_categories_id", getByCategory);
-// router.get("/:product_size_id", getBySize);
 router.get("/search", search);
 router.delete("/:id", deleteItem);
 router.put("/:id", updateItem);
