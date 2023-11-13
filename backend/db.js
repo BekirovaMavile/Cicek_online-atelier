b1fe647c7534cafc97a0a0865cfa3756aee7fd16
Ibraim Ganiev <ganiev.i.i.21@gmail.com>
2023-11-13 14:33:18 +0300

    Исправления профиля

--- backend/db.js
+++ backend/db.js
@@ -12,8 +12,8 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
     'atelierdb', // Название базы данных
-    'Ibraim', // Пользователь базы данных
-    'Atelier_0809', // Пароль от БД
+    'root', // Пользователь базы данных
+    'root', // Пароль от БД
     {
         host: 'localhost',
         dialect: 'mysql'
