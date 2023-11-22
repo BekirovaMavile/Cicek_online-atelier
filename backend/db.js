2ad0336716d9d8445bfca34f2a80d8e9161e1fab
BekirovaMavile <94888656+BekirovaMavile@users.noreply.github.com>
2023-11-22 21:00:11 +0300

    netlify

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,13 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
     'atelierdb', // Название базы данных
-    'root', // Пользователь базы данных
-    'mavile3103', // Пароль от БД
+    // 'root', // Пользователь базы данных
+    'vo5vxosn13r7j8hvg8wq',
+    // 'mavile3103', // Пароль от БД
+    'pscale_pw_ArnBHcNt40f4OBbw2P7PlBSDWuisNEnSNteU34VH65m',
     {
-        host: 'localhost',
+        // host: 'localhost',
+        host: 'mysql://vo5vxosn13r7j8hvg8wq:pscale_pw_ArnBHcNt40f4OBbw2P7PlBSDWuisNEnSNteU34VH65m@aws.connect.psdb.cloud/atelierdb?ssl={"rejectUnauthorized":true}',
         dialect: 'mysql'
     }
 );
